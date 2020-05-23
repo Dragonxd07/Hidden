cdb18324a2de9f4b33564d0ae022bdc162a61615
Dragonxd09 <56981154+Dragonxd09@users.noreply.github.com>
2020-05-23 02:43:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,40 @@
+// require the discord.js module
 const Discord = require('discord.js');
 
+// create a new Discord client
 const client = new Discord.Client();
 
- 
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Online!');
+client.user.setActivity('/help', { type: 'LISTENING' });
 
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
-});
-
- 
 
 client.on('message', message => {
+	console.log(message.content);
 
-    if (message.content === 'ping') {
+if (message.content === '/help') {
+	// send back "Pong." to the channel the message was sent in
+	message.channel.send('Hidden Bot Help \n /hidden - Gives You The Link For Hidden On Roblox \n /version - Shows You Hidden Bots Version \n /group - Gives You The Link To The Roblox Group \n /help - Shows Help');
+}
 
-       message.reply('pong');
+if (message.content === '/hidden') {
+	message.channel.send('https://www.roblox.com/games/5005452599/Hidden-beta?refPageId=dccf56fc-08e0-4142-ac95-d1d8e67b7800');
+}
 
-       }
+if (message.content === '/version') {
+	message.channel.send('ver: 1.0');
+}
+
+if (message.content === '/group') {
+	message.channel.send('https://www.roblox.com/groups/6392714/Billionaire-Production#!/about');
+}
 
 });
 
- 
+});
 
-// THIS  MUST  BE  THIS  WAY
+// login to Discord with your app's token
 
-client.login(process.env.Qi7mlDMcR3fzhvSc4lOCxgXNFEDY4Vf2);//BOT_TOKEN is the Client Secret
+client.login('NzEzNjEyNDM3ODA2NjQ1MjQ4.XsivcA.OzFjA05ZG3vlvzb432NDelZxeh4');
